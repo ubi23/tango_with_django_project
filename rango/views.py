1e4e386de9fd635d7e7c450101ea05907704bd14
Ubaidullah Khan <2271135k@student.gla.ac.uk>
2018-02-09 18:26:29 +0000

    chapter 10 finished

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 
@@ -17,17 +18,22 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+    visitor_cookie_handler(request)
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     context_dict = { 'categories': category_list,
-                     'pages': page_list}
+                     'pages': page_list,
+                     'visits':request.session['visits']}
+
+    response = render(request, 'rango/index.html', context=context_dict)
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+    return response
     
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
@@ -73,7 +79,7 @@ def add_category(request):
 			return index(request)
 		else:
 			print(forms.errors)
-
+            
 	return render(request, 'rango/add_category.html', {'form':form})
 
 
@@ -162,12 +168,8 @@ def user_login(request):
                 login(request, user)
                 return HttpResponseRedirect(reverse('index'))
             else:
-                return HttpResponse("Your Rango account is disabled.")
-        else:
-            print("Invalid login details: {0}, {1}".format(username, password))
-            return HttpResponse("Invalid login details supplied.")
-        
-        
+                
+                return HttpResponse("Invalid login details supplied.")
 
     else:
         return render(request, 'rango/login.html', {})
@@ -175,18 +177,44 @@ def user_login(request):
 @login_required
 def user_logout(request):
     logout(request)
-    return HttpResponseRedirect(reverse('index'))
+    return render(request,'rango/index.html',{})
 
 
 @login_required
 def restricted(request):
-    context_dict = {'message': "Since you're logged in, you can see this text!" }
+    context_dict = {}
     return render(request, 'rango/restricted.html', context_dict)
         
 def about(request):
-
-    context_dict = {}
     
-    return render(request, 'rango/about.html', context=context_dict)
+    
+    return render(request, 'rango/about.html',context=None)
 
 
+def visitor_cookie_handler(request):
+    # get the number of visits to the site
+    # default value is 1
+    visits = int(get_server_side_cookie(request,'visits',1))
+    last_visit_cookie = get_server_side_cookie(request,
+                                        'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    # if it's been more than a day since the last visit..
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    # update visits cookie
+    request.session['visits'] =  visits
+
+# helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val 
